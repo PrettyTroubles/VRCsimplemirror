75806fd5cf3e2e1ca624b52c378f2c0fedd635e2
PrettyTroubles <90886399+PrettyTroubles@users.noreply.github.com>
2025-06-19 18:19:17 -0700

    Add files via upload
    Hey Just some simple mirror buttons you can use for your VRC world.

---
+++ SimpleVRCMirror.cs
@@ -0,0 +1,18 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorToggle : UdonSharpBehaviour
+{
+    public GameObject mirrorObject;
+
+    public override void Interact()
+    {
+        if (mirrorObject != null)
+        {
+            mirrorObject.SetActive(!mirrorObject.activeSelf);
+        }
+    }
+}
+
